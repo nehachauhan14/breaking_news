bb00f624e075108bd1f38b45b756ea8673358906
Neha.Singh <nehachauhan79@gmail.com>
2020-08-12 20:04:41 +0530

    initial setup

---
+++ src/js/components/Form.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState(() => {
+      return {
+        value
+      };
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+          <form>
+            <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            />
+        </form>
+        <div>
+            {this.state.value}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Form;
+
+const wrapper = document.getElementById("container");
+wrapper ? ReactDOM.render(<Form />, wrapper) : false;
